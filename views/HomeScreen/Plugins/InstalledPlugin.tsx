62ee8201b2462b170c751d074b201572cdc74a54
nassipkaliv <nassipkaliv@gmail.com>
2023-10-10 01:08:01 +0600

    InstsalledPlugin done!

--- views/HomeScreen/Plugins/InstalledPlugin.tsx
+++ views/HomeScreen/Plugins/InstalledPlugin.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Switch, StyleSheet, TouchableOpacity, Image, ImageBackground, ScrollView } from 'react-native';
+import { View, Text, Linking, StyleSheet, TouchableOpacity, Image, ScrollView, } from 'react-native';
 import Font from '../../../components/Font';
 import GradientText from '../../../components/GradientText';
 import ProfileDetails from '../../../components/ProfileDetails';
 
 const InstalledPlugin: React.FC = ({ }) => {
 
+  const [isInstalled, setIsInstalled] = useState(false);
+
+  const handleInstallClick = () => {
+    setIsInstalled(!isInstalled); 
+  };
 
   return (
     <ScrollView style={styles.container}>
@@ -16,16 +21,77 @@ const InstalledPlugin: React.FC = ({ }) => {
           style={styles.delete}
         />
         <TouchableOpacity style={styles.checkBtn}>
-          <Text style={styles.btnText}>Install</Text>
+          <Text style={styles.btnText}>{isInstalled ? 'Enable' : 'Install'}</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.pluginContainer}>
         <View style={styles.pluginHeader}>
-          <Text style={styles.pluginName}>Plugin Name:</Text>
+          <Text style={styles.pluginTitle}>Plugin Name:</Text>
           <Text style={styles.pluginTopic}>Willpower Development</Text>
             <GradientText colors={['#9BD683', '#FBEC67']} style={styles.checkText}>
-              Not installed
+              {isInstalled ? 'Installed' : 'Not installed'}
             </GradientText>
+        </View>
+        
+        <View style={styles.pluginDescription}>
+          <Text style={styles.pluginTitle}>Description:</Text>
+          <Text style={styles.startText}>Simple steps to develop strong willpower.</Text>
+          <Text style={styles.paragraph}>
+            The main ideas of this plugin are: {"\n"}
+            1. Choose your willpower challenge. It might be habit that you want to brake or doing something that you have been avoiding or some other thing that specific only for you. {"\n"}
+            2. Start using technics that increase your willpower:
+          </Text>
+          <View style={styles.listContainer}>
+            <View style={styles.list}>
+              <Text style={styles.listBullet}>•</Text>
+              <Text style={styles.listText}>Practice simple meditation that increases your mindfulness.</Text>
+            </View>
+            <View style={styles.list}>
+              <Text style={styles.listBullet}>•</Text>
+              <Text style={styles.listText}>Every day track you decisions about your willpower challenge.</Text>
+            </View>
+            <View style={styles.list}>
+              <Text style={styles.listBullet}>•</Text>
+              <Text style={styles.listText}>When feeling temptation or stress, start doing one of the technics (corresponding tasks in the plugin) that can help you resist temptation and overcome stress.</Text>
+            </View>
+            <View style={styles.list}>
+              <Text style={styles.listBullet}>•</Text>
+              <Text style={styles.listText}>Every evening write your fillings about willpower during a day to a dairy </Text>
+            </View>
+          </View>
+
+          <Text style={styles.paragraph}>
+            This plugin is based on the Kelly McGonigal book
+          </Text>
+          <TouchableOpacity onPress={() => Linking.openURL('https://www.amazon.com/Willpower-Instinct-Self-Control-Works-Matters/dp/1583335080')}>
+            <Text style={styles.linkText}>The Willpower Instinct: How Self-Control Works, Why It Matters, and What You Can Do to Get More of It</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.pluginTasks}>
+          <Text style={styles.pluginTitle}>Tasks:</Text>
+          <Text style={styles.startText}>At start date:</Text>
+          <View style={styles.tasksList}>
+            <Text style={styles.listBullet}>•</Text>
+            <Text style={styles.listText}>Choose your willpower challenge</Text>
+          </View>
+          <Text style={styles.startText}>Daily</Text>
+          <View style={styles.tasksList}>
+            <Text style={styles.listBullet}>•</Text>
+            <Text style={styles.listText}>Meditation on breath</Text>
+          </View>
+          <View style={styles.tasksList}>
+            <Text style={styles.listBullet}>•</Text>
+            <Text style={styles.listText}>When feel temptation make 10 slow deep breath</Text>
+          </View>
+          <View style={styles.tasksList}>
+            <Text style={styles.listBullet}>•</Text>
+            <Text style={styles.listText}>When feel temptation delay your decision for 10 minutes</Text>
+          </View>
+          <View style={styles.tasksList}>
+            <Text style={styles.listBullet}>•</Text>
+            <Text style={styles.listText}>Before go to sleep write your fillings about willpower during a day to a dairy</Text>
+          </View>
         </View>
       </View>
     </ScrollView>
@@ -63,10 +129,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
   },
   pluginHeader: {
-    marginVertical: 5,
+    marginVertical: 0,
     paddingHorizontal: 0,
   },
-  pluginName: {
+  pluginTitle: {
     color: '#5a5a5a',
     fontSize: 12,
     fontFamily: Font["Gilroy-Regular"],
@@ -82,6 +148,58 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontFamily: Font["Gilroy-Regular"],
     lineHeight: 33.5,
+    opacity: 0,
+  },
+  pluginDescription: {
+    paddingHorizontal: 0,
+    marginBottom: 20,
+  },
+  startText: {
+    color: '#fff',
+    fontSize: 12,
+    fontFamily: Font["Gilroy-Regular"],
+    lineHeight: 33.5,
+  },
+  paragraph: {
+    color: '#fff',
+    fontSize: 12,
+    fontFamily: Font["Gilroy-Regular"],
+    lineHeight: 23.5,
+  },
+  listContainer: {
+    marginBottom: 40,
+  },
+  list: {
+    marginLeft: 120,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  listBullet: {
+    marginRight: 5,
+    color: '#fff',
+    fontSize: 12,
+    fontFamily: Font["Gilroy-Regular"],
+    lineHeight: 23.5,
+  },
+  listText: {
+    color: '#fff',
+    fontSize: 12,
+    fontFamily: Font["Gilroy-Regular"],
+    lineHeight: 23.5,
+  },
+  linkText: {
+    color: '#9977DC',
+    fontSize: 12,
+    fontFamily: Font["Gilroy-Regular"],
+    lineHeight: 23.5,
+    textDecorationLine: 'underline',
+  },
+  pluginTasks: {
+    marginBottom: 20,
+  },
+  tasksList: {
+    display: 'flex',
+    flexDirection: 'row',
   }
 });
 
