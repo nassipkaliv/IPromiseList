2fb66e5563c6be53571044285353861286b3834a
nassipkaliv <nassipkaliv@gmail.com>
2023-10-09 23:19:50 +0600

    ProfileScreen done!

--- views/ProfileScreen/ProfileScreen.tsx
+++ views/ProfileScreen/ProfileScreen.tsx
@@ -2,12 +2,24 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button, Switch, StyleSheet, TouchableOpacity, Image, ImageBackground } from 'react-native';
 import ProfileDetails from '../../components/ProfileDetails';
 import Font from '../../components/Font';
+import GradientText from '../../components/GradientText';
 
 const ProfileScreen: React.FC = ({ }) => {
-  
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
+  const [subscription, setSubscription] = useState("Pro Plan 👑");
+  const [subscriptionAction, setSubscriptionAction] = useState("Unsubscribe...");
+
+  const handleUnsubscribe = () => {
+    if (subscriptionAction === "Unsubscribe...") {
+      setSubscription("Free");
+      setSubscriptionAction("Subscribe to Pro Plan...");
+    } else {
+      setSubscription("Pro Plan 👑");
+      setSubscriptionAction("Unsubscribe...");
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -49,6 +61,28 @@ const ProfileScreen: React.FC = ({ }) => {
             value={email}
           />
         </View>
+        
+        <TouchableOpacity style={styles.changePass}>
+          <GradientText colors={['#9BD683', '#FBEC67']} style={styles.btnText}>
+            Change password...
+          </GradientText>
+        </TouchableOpacity>
+
+        <Text style={styles.subsTitle}>Subscription Plan</Text>
+          <View style={styles.subscriptionRow}>
+            <Text style={styles.plan}>{subscription}</Text>
+            <TouchableOpacity style={styles.subBtn} onPress={handleUnsubscribe}>
+              <GradientText colors={['#9BD683', '#FBEC67']} style={styles.btnText}>
+                {subscriptionAction}
+              </GradientText>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity style={styles.logoutBtn}>
+              <GradientText colors={['#9BD683', '#FBEC67']} style={styles.logoutText}>
+                Logout
+              </GradientText>
+          </TouchableOpacity>
       </View>
     </View>
   );
@@ -57,7 +91,7 @@ const ProfileScreen: React.FC = ({ }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#121014'
+    backgroundColor: '#121014',
   },
   editContainer: {
     position: 'absolute',
@@ -93,7 +127,7 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
   },
   input: {
-    height: 57,
+    height: 46,
     width: '100%',
     marginBottom: 10,
     paddingLeft: 10,
@@ -102,8 +136,55 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderRadius: 11,
     fontFamily: Font["Gilroy-Medium"],
-    color: '#fff'
+    color: '#fff',
   },
+  changePass: {
+    marginLeft: 20,
+    marginTop: 10,
+    width: '100%',
+  },
+  btnText: {
+    fontSize: 12,
+    fontFamily: Font["Gilroy-Medium"],
+    lineHeight: 33.5,
+    opacity: 0,
+    width: '100%',
+  },
+  subscriptionRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: 20,
+  },
+  subsTitle: {
+    color: '#5A5A5A',
+    fontSize: 12,
+    fontFamily: Font["Gilroy-Regular"],
+    marginLeft: 20,
+    marginTop: 10,
+  },
+  subBtn: {
+    width: '70%', 
+    marginLeft: 80,
+    paddingTop: 20,
+  },
+  plan: {
+    color: '#fff',
+    fontFamily: Font["Gilroy-Regular"],
+    fontSize: 14,
+    lineHeight: 33.5,
+  },
+  logoutBtn: {
+    marginLeft: 20,
+    marginTop: 20,
+  },
+  logoutText: {
+    fontFamily: Font["Gilroy-Medium"],
+    opacity: 0,
+    fontSize: 14,
+    lineHeight: 33.5
+  }
 });
 
 export default ProfileScreen;
